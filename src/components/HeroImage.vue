a31e330b680c9cc3b4aafd7558e45e8cd37c3de4
wolvgvng <jacekobst1@gmail.com>
2018-12-06 01:19:37 +0100

    First view finished

---
+++ src/components/HeroImage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="heroWrapper">
+    <img class="heroImage" />
+    <div class="heroAfter"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeroImage',
+};
+</script>
+
+<style lang="scss" scoped>
+  .heroImage {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    width: 100%;
+    height: 100vh;
+    background-image: url('../assets/heroimage.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: 80% 0;
+  }
+
+  .heroAfter {
+    content: '';
+    background-color: rgba(0,0,0, .4);
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    width: 100%;
+    height: 100vh;
+  }
+</style>
--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,34 +1,76 @@
 <template>
-  <div class="searchWrapper">
     <input
       id="search"
       name="search"
-      v-model="searchValue"
-      @input="handleInput"
+      :class="{ dark }"
       placeholder="e.g. Titan"
+      :value="value"
+      @input="handleChange"
     />
-  </div>
 </template>
 
 <script>
 export default {
   name: 'SearchInput',
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    dark: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    handleChange(e) {
+      this.$emit('input', e.target.value);
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-  .searchWrapper{
+
+  input {
     margin-top: 50px;
     display: flex;
     flex-direction: column;
-    width: 300px;
-  }
+    width: 250px;
+    z-index: 2;
 
-  input{
+    color: white;
+    font-size: 18px;
     height: 30px;
     border: 0;
-    border-bottom: 1px solid;
+    font-weight: 300;
+    border-bottom: 1px solid white;
+    transition: box-shadow .3s ease-out;
     text-align: center;
     background: none;
+
+    @media (min-width: 1024px) {
+      font-weight: 400;
+    }
+  }
+
+  input::placeholder{
+    color: white;
+    opacity: .4;
+  }
+
+  input:focus {
+    outline: none;
+    box-shadow: 0 10px 20px -8px rgba(255,255,255, .4);
+  }
+
+  .dark{
+   color: #1e3d4a;
+    border-bottom-color: #1e3d4a;
+  }
+
+  .dark:focus {
+    outline: none;
+    box-shadow: 0 10px 20px -8px rgba(#1e3d4a, .2);
   }
 </style>
